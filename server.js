4939ae91e1d192a689f06eac1626e391f2d026d6
Jeganath.B <87298700+Jeganath18@users.noreply.github.com>
2025-07-06 01:23:09 +0530

    Remove CORS

--- server.js
+++ server.js
@@ -15,10 +15,7 @@ const crypto = require("crypto");
 dotenv.config();
 
 const app = express();
-app.use(cors({
-  origin: "http://localhost:8080", // ✅ allow your frontend origin only
-  credentials: true, // ✅ allow cookies
-}));
+app.use(cors({}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
