6ff452dbf511697255658c6bf252fddac14218e1
wealthEmpire-code <wealthEmpire@1>
2025-07-21 03:38:01 +0530

    Add Logo and remove unwanted certification

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(
   session({
